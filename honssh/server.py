2b9b2aac754faf194334c681311f1d0298dc6f74
Thomas Nicholson <tnnich@googlemail.com>
2014-02-17 07:10:40 +0000

    File download fixed

--- honssh/server.py
+++ honssh/server.py
@@ -129,16 +129,16 @@ class HonsshServerTransport(transport.SSHServerTransport):
                         log.msg("Entered command: %s" % (self.command))
                         txtlog.log(self.txtlog_file, "Entered command: %s" % (self.command))
                         if self.cfg.get('extras', 'file_download') == 'true':
-                            file = re.match("wget (.+)((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)", self.command)
+                            file = re.match("wget(.+)((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)", self.command)
                             if file:
-                                
+
                                 if not os.path.exists('downloads'):
                                     os.makedirs('downloads')
-                                    os.chmod('downloads',0755)                                
-                                
+                                    os.chmod('downloads',0755)                   
+
                                 txtlog.log(self.txtlog_file, "wget Download Detected - Downloading File Using: %s" % str(file.group(0)))
                                 wgetCommand = "wget -P downloads " + str(file.group(1)) + " " + str(file.group(2))
-                                subprocess.Popen(wgetCommand)
+                                subprocess.Popen(wgetCommand, shell=True)
 
                         self.command = ""
                     elif data == '\x7f':    #if backspace
