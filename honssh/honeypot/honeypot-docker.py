d64838e4a3c25d4ec910475d10f2452e128a9a53
bang-uin <bang-uin@air-snitch.de>
2016-12-09 01:18:53 +0100

    * change: replaced twisted inotify with watchdog to improve fs_watch startup * change: added watchdog to requirements (cherry picked from commit a0c7310)

--- honssh/honeypot/honeypot-docker.py
+++ honssh/honeypot/honeypot-docker.py
@@ -28,18 +28,16 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 import os
-import time
-
-from honssh.config import Config
-from honssh.utils import validation
-from honssh import spoof, log
 
 from docker import Client
-
-from honssh.honeypot.docker_utils import docker_cleanup
 from twisted.python import filepath
-from twisted.internet import inotify
-from library_fixes.twisted import inotifyFix
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+from honssh import spoof, log
+from honssh.config import Config
+from honssh.honeypot.docker_utils import docker_cleanup
+from honssh.utils import validation
 
 
 class Plugin(object):
@@ -229,21 +227,19 @@ class DockerDriver(object):
         return {"id": self.container_id, "ip": self.container_ip}
 
     def teardown_container(self, destroy_container):
+        if self.watcher is not None:
+            self.watcher.unschedule_all()
+            log.msg(log.LCYAN, '[PLUGIN][DOCKER]', 'Filesystem watcher stopped')
+
         self.connection.stop(self.container_id)
+        log.msg(log.LCYAN, '[PLUGIN][DOCKER]',
+                'Stopped container (%s, %s)' % (self.container_ip, self.container_id))
 
         # Check for container reuse
         if not self.reuse_container or destroy_container:
-            log.msg(log.LCYAN, '[PLUGIN][DOCKER]',
-                    'Destroying container (%s, %s)' % (self.container_ip, self.container_id))
             self.connection.remove_container(self.container_id, force=True)
-        else:
             log.msg(log.LCYAN, '[PLUGIN][DOCKER]',
-                    'Stopping container (%s, %s)' % (self.container_ip, self.container_id))
-
-        if self.watcher is not None:
-            self.watcher.stopReading()
-            self.watcher.loseConnection()
-            log.msg(log.LCYAN, '[PLUGIN][DOCKER]', 'Filesystem watcher stopped')
+                    'Destroyed container (%s, %s)' % (self.container_ip, self.container_id))
 
     def _file_get_contents(self, filename):
         with open(filename) as f:
@@ -270,8 +266,8 @@ class DockerDriver(object):
         storage_driver = docker_info['Driver']
 
         supported_storage = {
-            'aufs': '%s/%s/mnt/%s',  # -> /var/lib/docker/aufs/mnt/mount-id
-            'btrfs': '%s/%s/subvolumes/%s',  # -> /var/lib/docker/btrfs/subvolumes/mount-id
+            'aufs': '%s/%s/mnt/%s',  # -> /var/lib/docker/aufs/mnt/<mount-id>
+            'btrfs': '%s/%s/subvolumes/%s',  # -> /var/lib/docker/btrfs/subvolumes/<mount-id>
             'overlay': '%s/%s/%s/merged',  # -> /var/lib/docker/overlay/<mount-id>/merged
             'overlay2': '%s/%s/%s/merged'  # -> /var/lib/docker/overlay2/<mount-id>/merged
         }
@@ -286,10 +282,11 @@ class DockerDriver(object):
 
             try:
                 # Create watcher and start watching
-                self.watcher = inotifyFix.INotifyRFix()
-                self.watcher.startReading()
-                self.watcher.watch(filepath.FilePath(self.mount_dir), mask=(inotify.IN_CREATE | inotify.IN_MODIFY),
-                                   autoAdd=True, callbacks=[self.notify], recursive=True)
+                self.watcher = Observer()
+                event_handler = DockerFileSystemEventHandler(self.overlay_folder, self.mount_dir,
+                                                             self.max_filesize, self.use_revisions);
+                self.watcher.schedule(event_handler, self.mount_dir, recursive=True)
+                self.watcher.start()
 
                 log.msg(log.LGREEN, '[PLUGIN][DOCKER]', 'Filesystem watcher started')
             except Exception as exc:
@@ -298,45 +295,66 @@ class DockerDriver(object):
             log.msg(log.LRED, '[PLUGIN][DOCKER]',
                     'Filesystem watcher not supported for storage driver "%s"' % storage_driver)
 
-    def notify(self, ignored, file, mask):
-        # log.msg(log.LYELLOW, '[notify]', '%s' % str(file))
 
-        if mask & inotify.IN_CREATE or mask & inotify.IN_MODIFY:
-            if file.exists() and file.getsize() > 0:
-                # Check max_filesize constraint
-                if self.max_filesize == 0 or (self.max_filesize > 0 and (file.getsize() / 1024) <= self.max_filesize):
-                    # Construct src and dest path as string
-                    src_path = '%s/%s' % (file.dirname(), file.basename())
-                    dest_path = '%s/%s' % (self.overlay_folder, src_path.replace(self.mount_dir, ''))
+class DockerFileSystemEventHandler(FileSystemEventHandler):
+    def __init__(self, overlay_folder, mount_dir, max_filesize, use_revisions):
+        super(FileSystemEventHandler, self).__init__()
+        self.overlay_folder = overlay_folder
+        self.mount_dir = mount_dir
+        self.max_filesize = max_filesize
+        self.use_revisions = use_revisions
 
-                    # Create dest file object
-                    d = filepath.FilePath(dest_path)
+    def on_modified(self, event):
+        #log.msg(log.LYELLOW, '[FS_WATCH][on_modified]', '%s' % repr(event))
 
-                    # Check for revision constraint
-                    if self.use_revisions and d.exists():
-                        c = 0
-                        while True:
-                            # Increment counter and construct new path
-                            c += 1
-                            dpath = dest_path + "-" + str(c)
-                            d = filepath.FilePath(dpath)
+        if not event.is_directory:
+            self.process_event(event.src_path)
 
-                            # Check for new path
-                            if not d.exists():
-                                dest_path = dpath
-                                break
+    def on_moved(self, event):
+        #log.msg(log.LYELLOW, '[FS_WATCH][on_moved]', '%s' % repr(event))
 
-                    # log.msg(log.LBLUE, '[COPY]', '%s / %s' % (src_path, dest_path))
+        if not event.is_directory:
+            self.process_event(event.dest_path)
 
-                    try:
-                        # Create directory tree
-                        os.makedirs('%s%s' % (self.overlay_folder, file.dirname().replace(self.mount_dir, '')))
-                    except:
-                        # Ignore exception
-                        pass
+    def process_event(self, file_path):
+        file = filepath.FilePath(file_path)
+        #log.msg(log.LYELLOW, '[FS_WATCH][process_event]', '%s' % str(file))
 
-                    try:
-                        # Do actual copy
-                        file.copyTo(d)
-                    except Exception as exc:
-                        log.msg(log.LRED, '[PLUGIN][DOCKER][FS_WATCH]', 'FAILED TO COPY "%s" - %s' % (src_path, str(exc)))
+        if file.exists() and file.getsize() > 0:
+            # Check max_filesize constraint
+            if self.max_filesize == 0 or (self.max_filesize > 0 and (file.getsize() / 1024) <= self.max_filesize):
+                # Construct src and dest path as string
+                src_path = '%s/%s' % (file.dirname(), file.basename())
+                dest_path = '%s/%s' % (self.overlay_folder, src_path.replace(self.mount_dir, ''))
+
+                # Create dest file object
+                d = filepath.FilePath(dest_path)
+
+                # Check for revision constraint
+                if self.use_revisions and d.exists():
+                    c = 0
+                    while True:
+                        # Increment counter and construct new path
+                        c += 1
+                        dpath = dest_path + "-" + str(c)
+                        d = filepath.FilePath(dpath)
+
+                        # Check for new path
+                        if not d.exists():
+                            dest_path = dpath
+                            break
+
+                #log.msg(log.LBLUE, '[COPY]', '%s / %s' % (src_path, dest_path))
+
+                try:
+                    # Create directory tree
+                    os.makedirs('%s%s' % (self.overlay_folder, file.dirname().replace(self.mount_dir, '')))
+                except:
+                    # Ignore exception
+                    pass
+
+                try:
+                    # Do actual copy
+                    file.copyTo(d)
+                except Exception as exc:
+                    log.msg(log.LRED, '[PLUGIN][DOCKER][FS_WATCH]', 'FAILED TO COPY "%s" - %s' % (src_path, str(exc)))
